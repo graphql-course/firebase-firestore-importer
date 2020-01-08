6ee1872b591b9f183b6d334c1bc643ba0f1df4c8
Anartz Mugika Ledo <mugan86@gmail.com>
2020-01-08 19:45:16 +0100

    Version OK operativa

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv');
+
+const admin = require('./node_modules/firebase-admin');
+const serviceAccount = require("./permissions.json");
+const data = require('./assets/quiz.json');
+const collectionKey = 'quizes'; 
+//name of the collection
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.DATABASE
+});
+const firestore = admin.firestore();
+const settings = {timestampsInSnapshots: true};
+firestore.settings(settings);
+if (data && (typeof data === "object")) {
+    Object.keys(data).forEach(docKey => { 
+        firestore.collection(collectionKey).doc(docKey).set(data[docKey]).then((res) => {    
+            console.log("Document " + docKey + " successfully written!");}).catch((error) => {   
+            console.error("Error writing document: ", error);
+        });
+    });
+}
