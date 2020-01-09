ddc926ac9f42df2a50bebafbfa585f10d33295ed
Anartz Mugika Ledo <mugan86@gmail.com>
2020-01-09 19:24:18 +0100

    Update

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require('dotenv');
 
 const admin = require('./node_modules/firebase-admin');
 const serviceAccount = require("./permissions.json");
-const data = require('./assets/questions.json');
-const collectionKey = 'questions'; 
+const data = require('./assets/employers/employers.json');
+const collectionKey = 'employers'; 
 //name of the collection
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -14,7 +14,7 @@ const settings = {timestampsInSnapshots: true};
 firestore.settings(settings);
 if (data && (typeof data === "object")) {
     Object.keys(data).forEach(docKey => { 
-        firestore.collection(collectionKey).doc(String(parseInt(docKey) + 1)).set(data[docKey]).then((res) => {    
+        firestore.collection(collectionKey).doc(data[docKey].id).set(data[docKey]).then((res) => {  
             console.log("Document " + docKey + " successfully written!");}).catch((error) => {   
             console.error("Error writing document: ", error);
         });
